40b306d91155f86df4506ed3ddb922a8062f960b
Gloria <hyojung.gm@gmail.com>
2022-04-04 17:33:57 -0400

    multiple counter adding works

--- main.js
+++ main.js
@@ -36,17 +36,21 @@ function switch_image(x) {
 
 //https://stackoverflow.com/questions/55328748/how-to-store-and-retrieve-shopping-cart-items-in-localstorage
 function addCart(name, price) {
+  alert("Added to Cart!");
   let products = [];
   if(localStorage.getItem('products')){
     products = JSON.parse(localStorage.getItem('products'));
   }
-  products.push(new Product(name, price));
+  var x = document.getElementById("counter_count").value;
+  for (var i=0; i<x; i++) {
+    products.push(new Product(name, price));
+  }
   localStorage.setItem('products', JSON.stringify(products));
   loadCount();
 }
 
 function removeProduct(name, rowid){
-  alert("hello");
+  alert(`Are you sure you want to remove the ${name}?`);
   let storageProducts = JSON.parse(localStorage.getItem('products'));
   let products = storageProducts.filter(product => product.name !== name );
   localStorage.setItem('products', JSON.stringify(products));
@@ -70,49 +74,56 @@ function loadCart(){
   }
   //document.getElementById("shoppingcart").innerHTML = products;
   document.getElementById("shoppingcartlen").innerHTML = products.length;
-
   var shoppingTable = document.getElementById("shopping_table");
 
-  // correct the count
-  var pdict = {};
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].name in pdict) {
-       var x = pdict[products[i].name][0];
-       var y = pdict[products[i].name][1];
-       pdict[products[i].name] = [x+products[i].price, y+1];
-    }
-    else {
-      pdict[products[i].name] = [products[i].price, 1];
-    }
-  }
-
-  // display shopping cart
-  for(var key in pdict) {
-    var value = pdict[key];
-    var price = value[0];
-    var quantity = value[1];
-
+  if (products.length == 0){
+    console.log("nothing");
     var row =  shoppingTable.insertRow(1);
-    row.id = `${key}-row`;
-    var col1 = row.insertCell(0);
-    var col2 = row.insertCell(1);
-    var col3 = row.insertCell(2);
-    var col4 = row.insertCell(3);
-    col1.innerHTML = key;
-    col2.innerHTML = quantity;
-    col3.innerHTML = price;
-
-    var btn = document.createElement("button");
-    btn.onclick = function(){
-      removeProduct(key, row.id);
-    };
-    var t = document.createTextNode("Delete");
-    btn.appendChild(t);
-    col4.id = "placer";
-    //col4.innerHTML = btn;
-    document.getElementById('placer').appendChild(btn);
+    cell = row.insertCell(0);
+    cell.colSpan = "4";
+    cell.innerHTML = "No items in the cart!";
   }
+  else{
+    // correct the count
+    var pdict = {};
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].name in pdict) {
+         var x = pdict[products[i].name][0];
+         var y = pdict[products[i].name][1];
+         pdict[products[i].name] = [x+products[i].price, y+1];
+      }
+      else {
+        pdict[products[i].name] = [products[i].price, 1];
+      }
+    }
 
+    // display shopping cart
+    for(var key in pdict) {
+      var value = pdict[key];
+      var price = value[0];
+      var quantity = value[1];
+
+      var row =  shoppingTable.insertRow(1);
+      row.id = `${key}-row`;
+      var col1 = row.insertCell(0);
+      var col2 = row.insertCell(1);
+      var col3 = row.insertCell(2);
+      var col4 = row.insertCell(3);
+      col1.innerHTML = key;
+      col2.innerHTML = quantity;
+      col3.innerHTML = price.toFixed(2);
+
+      var btn = document.createElement("button");
+      btn.onclick = function(){
+        removeProduct(key, row.id);
+      };
+      var t = document.createTextNode("Delete");
+      btn.appendChild(t);
+      col4.id = "placer";
+      //col4.innerHTML = btn;
+      document.getElementById('placer').appendChild(btn);
+    }
+  }
 
 }
 
